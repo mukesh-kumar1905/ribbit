5eff0afeaea16c7863ae4e39caffc1b495ff7a87
Mukesh Kumar <mukeshkmar1905@gmail.com>
2014-04-13 22:13:00 +0530

    created usersController#new, #create methods; users/new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+	def new
+		@user=User.new
+	end
+	def create
+		@user=User.new(post_params(params))
+		if @user.save
+			redirect_to @user,notice:"Thank you for signing up for Ribbit."
+		else
+			render 'new'
+		end
+	end
+	def destroy
+
+	end
+	private
+	def post_params(params)
+		params.require(:user).permit(:avatar_url,:bio,:email,:name,:password,:password_confirmation ,:username)
+	end
 end
