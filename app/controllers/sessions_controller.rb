f8d0a755e48cefd27ce7e68e868295a53547d19f
Mukesh Kumar <mukeshkmar1905@gmail.com>
2014-04-13 22:58:29 +0530

    added login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,16 @@ class SessionsController < ApplicationController
   def create
   	user=User.find_by_username(params[:username])
   	if user and user.authenticate(params[:password])
-  		session[:user_id]=user.user_id
-  		redirect_to root_user,notice: "Logged In!"
+  		session[:user_id]=user.id
+  		redirect_to root_url,notice: "Logged In!"
   	else
   		flash[:error]="Wrong username/password"
-  		redirect_to root_user
+  		redirect_to root_url
   	end
   end
 
   def destroy
   	session[:user_id]=nil
-  	redirect_to root_user,notice: "Logged Out."
+  	redirect_to root_url,notice: "Logged Out."
   end
 end
