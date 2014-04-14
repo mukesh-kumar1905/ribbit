7b96fce49b0776830397bb5dcae39115c6e01c99
Mukesh Kumar <mukeshkmar1905@gmail.com>
2014-04-14 16:41:08 +0530

    added functionality to follow and unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 require 'digest/md5'
+
 class User < ActiveRecord::Base
 	has_secure_password
 
 	has_many :ribbits
 	
+	has_many :follower_relationships, class_name:'Relationship',foreign_key:'followed_id'
+	has_many :followed_relationships, class_name:'Relationship',foreign_key:'follower_id'
+	has_many :followers, through: :follower_relationships
+	has_many :followeds, through: :followed_relationships
+
 	before_validation :prep_email
 	before_save :create_avatar_url
 
@@ -11,6 +17,12 @@ class User < ActiveRecord::Base
 	validates :username , presence:true ,uniqueness: true
 	validates :name,presence: true
 
+	def following? user
+		self.followeds.include? user
+	end
+	def follow user
+		Relationship.create follower_id: self.id ,followed_id:user.id
+	end
 	private
 	def prep_email
 		self.email=self.email.strip.downcase if self.email
