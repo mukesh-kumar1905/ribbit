3b3fa3f04f0031070feefc1fae946993b8109c50
Mukesh Kumar <mukeshkmar1905@gmail.com>
2014-04-14 22:40:43 +0530

    added page for public profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,11 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def index
+		@users=User.all
+		@ribbit=Ribbit.new
+	end
+
 	def show
 		@user=User.find(params[:id])
 		@ribbit=Ribbit.new
