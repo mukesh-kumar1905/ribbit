9e9f63b9c43dd9b920dbe64ff92c3cfc2ac0defc
Mukesh Kumar <mukeshkmar1905@gmail.com>
2014-04-14 14:51:14 +0530

    now saving ribbits

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,2 +1,15 @@
 class RibbitsController < ApplicationController
+
+	def create
+		ribbit=Ribbit.new(post_params(params))
+		ribbit.user_id=current_user.id
+		flash[:error]="Your Ribbit was over 140 characters." unless ribbit.save
+		redirect_to root_url
+	end
+
+	private
+	def post_params(params)
+		params.require(:ribbit).permit(:user_id,:content)
+	end
+
 end
